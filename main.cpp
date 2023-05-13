dea58d2b3e2dceca5bdc6233fd4b8543ccba4e25
Faruk Uysal <faruk.uysal@tigahealth.com>
2023-05-13 18:33:08 +0300

    Setting up renderer,texture,buffer

--- main.cpp
+++ main.cpp
@@ -11,12 +11,38 @@ int main(int argc, char* args[]) {
         cout<< "SDL init failed." << endl;
         return 1;
     }
+
     SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
 
     if(window == NULL){
         SDL_Quit();
         return 2;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1, SDL_RENDERER_PRESENTVSYNC); // Rendering is synchronized with the screen refresh rate
+    SDL_Texture* texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STATIC,SCREEN_WIDTH,SCREEN_HEIGHT);
+    if (renderer == NULL){
+        cout<<"Could not create renderer"<<endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+    if (texture == NULL){
+        cout<<"Could not create texture"<<endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 4;
+    }
+
+    Uint32 *buffer = new Uint32[SCREEN_HEIGHT*SCREEN_WIDTH];
+
+    memset(buffer,0xFF,SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+    SDL_UpdateTexture(texture,NULL,buffer,SCREEN_WIDTH* sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer,texture,NULL,NULL);
+    SDL_RenderPresent(renderer);
 
     bool quit = false;
 
@@ -34,6 +60,9 @@ int main(int argc, char* args[]) {
         }
     }
 
+    delete [] buffer;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
