9f193da95451d408c37bb2d30c7394292df9fd1d
Faruk Uysal <faruk.uysal@tigahealth.com>
2023-05-15 17:33:22 +0300

    Adding color change algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 #include "Screen.h"
+#include<math.h>
+
 using namespace std;
 using namespace farukprogramming;
 
@@ -14,12 +16,18 @@ int main(int argc, char* args[]) {
     while(true){
         // Update particles
         // Draw particles
+
+        // Color changing algorithm
+        int elapsed = SDL_GetTicks();
+        unsigned char green =(unsigned char) ((1 + sin(elapsed * 0.0005))*128);
+        unsigned char red =(unsigned char) ((1 + sin(elapsed * 0.001))*128);
+        unsigned char blue =(unsigned char) ((1 + sin(elapsed * 0.0003))*128);
+
         for(int y =0;y< Screen::SCREEN_HEIGHT;y++){
             for(int x = 0; x< Screen::SCREEN_WIDTH;x++){
-                screen.setPixel(x,y,128,0,255);
+                screen.setPixel(x,y,red,green,blue);
             }
         }
-
         //Draw the screen
         screen.update();
 
