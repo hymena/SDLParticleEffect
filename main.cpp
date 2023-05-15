8c51052424ec716b3030cabf14190f20318287b5
Faruk Uysal <faruk.uysal@tigahealth.com>
2023-05-15 17:37:04 +0300

    test

--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@ int main(int argc, char* args[]) {
         unsigned char red =(unsigned char) ((1 + sin(elapsed * 0.001))*128);
         unsigned char blue =(unsigned char) ((1 + sin(elapsed * 0.0003))*128);
 
+
+        cout<<"testing github thing"<<endl;
         for(int y =0;y< Screen::SCREEN_HEIGHT;y++){
             for(int x = 0; x< Screen::SCREEN_WIDTH;x++){
                 screen.setPixel(x,y,red,green,blue);
