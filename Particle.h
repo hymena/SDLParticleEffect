e2d9dfe5fd79d054cbe286e91d8f7c7f480691d3
Faruk Uysal <uysal0113@gmail.com>
2023-06-02 20:05:25 +0300

    particle motion

--- Particle.h
+++ Particle.h
@@ -10,12 +10,16 @@ namespace farukprogramming {
     struct Particle {
         double m_x;
         double m_y;
+    private:
         double m_speed;
         double m_direction;
+    private:
+        void init();
     public:
         Particle();
         virtual ~Particle();
         void update(int interval);
+
     };
 
 } // farukprogramming
